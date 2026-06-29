bbfaa22cf87f8a03a049734e0174df1accebd861
Yin,KyoungSoo <yinks94@gmail.com>
2026-06-29 10:49:14 +0900

    first commit(20260629)

---
+++ src/test/java/kr/ink94/dgehrm/DgehrmApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ink94.dgehrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgehrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
